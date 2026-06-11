54a9ca27029f369890fb19e2d1693f8705717ba1
Florian Stevener <f.stevener@web.de>
2026-06-11 20:42:46 +0200

    made tweet data ready for ml pipeline

---
+++ src/tweet_utils.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+# import torch
+# from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoModel, pipeline
+# import string
+# from datetime import datetime
+
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.decomposition import LatentDirichletAllocation
+
+# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize
+
+# nltk.download("punkt")
+# nltk.download("stopwords")
+
+# STOPWORDS = set(stopwords.words("english"))
+# vader = SentimentIntensityAnalyzer()
+
+# from sentence_transformers import SentenceTransformer
+# from transformers import pipeline
+
+from sklearn.metrics.pairwise import cosine_similarity
+# from sklearn.decomposition import PCA
+# from sklearn.cluster import KMeans
+# from sklearn.preprocessing import StandardScaler
+
+# from bertopic import BERTopic
+
+# warnings.filterwarnings("ignore")
+
